ba935bd556481125a37f27921008ec9e8af05523
ERASTTT <82580195+ERASTTT@users.noreply.github.com>
2022-10-04 10:49:22 +0500

    Add files via upload

---
+++ Рзаев/script.js
@@ -0,0 +1,30 @@
+//Задание1
+var randomBodyParts = ["ключица", "усики", "череп"];
+var randomAdjectives = ["вонючая", "унылая", "дурацкая"];
+var randomWords = ["муха", "выдра", "дубина", "мартышка", "крыса"];
+// Выбор случайной части тела из массива randomBodyParts:
+var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
+// Выбор случайного прилагательного из массива randomAdjectives:
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
+// Выбор случайного слова из массива randomWords:
+var randomWord = randomWords[Math.floor(Math.random() * 5)];
+// Соединяемслучайные строки в предложение:
+var randomInsult = "У тебя " + randomBodyPart + " словно " + 
+randomAdjective + " " + randomWord + "!!!";
+document.write(randomInsult);
+"У тебя нос словно дурацкая выдра!!!"
+//Задание 2
+var randomBodyParts = ["ключица", "усики", "череп"];
+var randomAdjectives = ["вонючая", "унылая", "дурацкая"];
+var randomWords = ["тварь", "выдра", "дубина", "мартышка", "крыса"];
+// Выбор случайной части тела из массива randomBodyParts:
+var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
+// Выбор случайного прилагательного из массива randomAdjectives:
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
+// Выбор случайного слова из массива randomWords:
+var randomWord = randomWords[Math.floor(Math.random() * 5)];
+// Соединяемслучайные строки в предложение:
+var randomInsult = "У тебя " + randomBodyPart + " словно " + 
+randomAdjective + " " + randomWord + "!!!";
+document.write(randomInsult);
+"У тебя нос словно дурацкая выдра!!!"
